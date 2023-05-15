6f62f2ea1c5a6793fed7f0ca4a8101d619ba37e5
JoshMJ <jmjaffe@microsoft.com>
2023-05-15 13:54:18 -0700

    Input args now specified with flags

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-content_dir=$1
-public_key=$2
-private_key=$3
+# Pull directory to sign, public key, and secret key from input args
+while getopts d:s:p: flag
+do
+    case "${flag}" in
+        d) content_dir=${OPTARG};;
+        s) secret_key_file=${OPTARG};;
+        p) public_key_file=${OPTARG};;
+    esac
+done
 
 # These steps were derived from the adoptium documentation found at:
 # https://adoptium.net/docs/marketplace-listing/
 for json in $(ls ${content_dir}/*.json); do
     echo "$json"
     # Generate signature
-    openssl dgst -sha256 -sign $private_key -out ${json}.sig $json
+    openssl dgst -sha256 -sign $secret_key_file -out ${json}.sig $json
 
     # Verify
-    openssl dgst -sha256 -verify $public_key -signature ${json}.sig $json
+    openssl dgst -sha256 -verify $public_key_file -signature ${json}.sig $json
 
     # Base64 encode for publishing
     cat ${json}.sig | base64 -w 0 > ${json}.sha256.sign
