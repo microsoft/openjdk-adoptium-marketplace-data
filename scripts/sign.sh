13a155dc19efbd7ae4bcafefd08992c059cfc349
jmjaffe37 <111303274+jmjaffe37@users.noreply.github.com>
2023-05-16 09:52:39 -0700

    Apply suggestions from code review
    Co-authored-by: George Adams <george.adams@microsoft.com>

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -7,8 +7,8 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         -h|--help) HELP=true ;;
         -d|--directory) content_dir="$2"; shift ;;
-        -s|--secret-key) secret_key_file=$2; shift ;;
-        -p|--public-key) public_key_file=$2; shift ;;
+        -s|--secret-key) secret_key_file="$2"; shift ;;
+        -p|--public-key) public_key_file="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -32,10 +32,10 @@ fi
 for json in $(ls ${content_dir}/*.json); do
     echo "$json"
     # Generate signature
-    openssl dgst -sha256 -sign $secret_key_file -out ${json}.sig $json
+    openssl dgst -sha256 -sign "$secret_key_file" -out ${json}.sig $json
 
     # Verify
-    openssl dgst -sha256 -verify $public_key_file -signature ${json}.sig $json
+    openssl dgst -sha256 -verify "$public_key_file" -signature ${json}.sig $json
 
     # Base64 encode for publishing
     cat ${json}.sig | base64 -w 0 > ${json}.sha256.sign
