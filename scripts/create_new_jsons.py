69b40ba51568946dd12b77331ffbb9b968ef5214
jmjaffe37 <jmjaffe@microsoft.com>
2025-02-26 15:53:43 -0800

    switched to jinja2

--- scripts/create_new_jsons.py
+++ scripts/create_new_jsons.py
@@ -28,155 +28,91 @@ import sys
 from datetime import datetime
 from pathlib import Path
 from typing import Tuple
-import requests
 
+import requests
 from docopt import docopt
+from jinja2 import Environment, FileSystemLoader
 
 logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 DATE_FORMAT = "%Y-%m-%dT%H:00:00Z"
 
 
-def create_new_json_file(
+def update_index_file(
     folder: str,
-    new_version: str,
-) -> str:
+    new_filename: str,
+):
     """
-    Create a new JSON file based on an existing one.
+    Ensure existing index file has entry for new PSU's json.
     Args:
         folder (str): The folder containing the JSON files.
-        new_version (str): The new version to be added.
-    Returns:
-        new_file_path (str): The path to the new JSON file.
+        new_filename (str): The name of the new JSON file.
     """
     # Read the existing JSON file
     index_file_path = Path(folder).joinpath("index.json")
     with index_file_path.open("r") as index_file:
         index_data = json.load(index_file)
 
-    # Get the filename from the "releases" key
-    old_filename = index_data["releases"][0]
-
-    # Get rid of the build number and replace '.' with '_'
-    formatted_version = new_version.split("+")[0].replace(".", "_")
-
-    # Define the new filename
-    new_filename = f"jdk_{formatted_version}.json"
-
-    # Copy the original file to the new file
-    old_file_path = Path(folder).joinpath(old_filename)
-    new_file_path = Path(folder).joinpath(new_filename)
-    new_file_path.write_text(old_file_path.read_text())
-
-    logger.info(f"Copied {old_filename} to {new_filename}")
-
     # Insert the new filename at the beginning of the "releases" list
-    index_data["releases"].insert(0, new_filename)
+    if new_filename not in index_data["releases"]:
+        index_data["releases"].insert(0, new_filename)
 
-    # Write the updated index data back to the index file
-    with index_file_path.open("w") as index_file:
-        json.dump(index_data, index_file, indent=4)
+        # Write the updated index data back to the index file
+        with index_file_path.open("w") as index_file:
+            json.dump(index_data, index_file, indent=4)
 
-    logger.info(f"Updated {index_file_path} with new entry {new_filename}")
+        logger.info(
+            f"Updated index file {index_file_path} with new entry {new_filename}"
+        )
 
-    return new_file_path.as_posix()
+    else:
+        logger.info(f"{new_filename} already exists in {index_file_path}")
 
 
-def update_new_json_info(
+def create_new_json(
     version: str,
-    file_path: str,
-) -> Tuple[str, str]:
-    """
-    Update the JSON file with new version information.
-    Args:
-        version (str):      The new version to be added.
-        file_path (str):    The path to the new JSON file.
-    Returns:
-        old_version (str):  The version from the most resent JSON file.
-        old_psu_tag (str):  The psu tag from the most resent JSON file.
-    """
-    # Load the contents of the new JSON file into a dictionary
-    file_path = Path(file_path)
-    with file_path.open("r") as new_file:
-        file_data = json.load(new_file)
-
-    # Setup needed vars
-    content = file_data["releases"][0]
-    today = datetime.now().strftime(DATE_FORMAT)
-    old_version = content["openjdk_version_data"]["openjdk_version"]
-    old_psu_tag = content["binaries"][0]["aqavit_results_link"].split("/")[-2]
-
-    # Update overall JSON data
-    content["release_name"] = f"jdk-{version}"
-    content["last_updated_timestamp"] = today
-
-    # Update openjdk_version_data
-    content["openjdk_version_data"]["major"] = int(version.split(".")[0])
-    content["openjdk_version_data"]["minor"] = int(version.split(".")[1])
-    content["openjdk_version_data"]["security"] = int(
-        version.split(".")[2].split("+")[0]
-    )
-    content["openjdk_version_data"]["build"] = int(version.split("+")[1])
-    content["openjdk_version_data"]["openjdk_version"] = version
-
-    # Update binary info
-    for entry in content["binaries"]:
-        if "timestamp" in entry:
-            entry["timestamp"] = today
-
-    # Write the updated contents back to the file
-    with file_path.open("w") as new_file:
-        json.dump(file_data, new_file, indent=4)
-
-    logger.info(
-        f"Updated {file_path} with new version info {version} and timestamp {today}"
-    )
-
-    return old_version, old_psu_tag
-
-
-def update_new_json_full_versions(
-    version: str,
-    old_version: str,
     psu_tag: str,
-    old_psu_tag: str,
-    file_path: str,
-):
+    folder: str,
+) -> Path:
     """
-    Update the full version numbers in the new JSON file.
+    Create the new JSON file for new PSU with updated info.
     Args:
         version (str): The new version to be added.
-        old_version (str): The version from the most resent JSON file.
         psu_tag (str): The psu tag for the new version.
-        old_psu_tag (str): The psu tag from the most resent JSON file.
-        file_path (str): The path to the new JSON file.
+        folder (str): The folder containing the JSON files.
+    Returns:
+        new_file_path (Path): The path to the new JSON file.
     """
-    # Load the contents of the new JSON file into a dictionary
-    file_path = Path(file_path)
-    with file_path.open("r") as f:
-        file_data_str = f.read()
+    path = str(Path(__file__).parent.parent.joinpath("templates"))
+    env = Environment(loader=FileSystemLoader(path))
+    template = env.get_template("jdk_version_template.json")
 
-    # Replace all instances of old_version with version
-    ## Replace full versions with '+' in it. Example: 11.0.25+9 -> 11.0.26+4
-    file_data_str = file_data_str.replace(old_version, version)
-    ## Replace full versions with '_' in it. Example: 11.0.25_9 -> 11.0.26_4
-    file_data_str = file_data_str.replace(
-        old_version.replace("+", "_"), version.replace("+", "_")
-    )
-    ## Replace full versions without build numbers. Example: 11.0.25 -> 11.0.26
-    file_data_str = file_data_str.replace(
-        old_version.split("+")[0], version.split("+")[0]
-    )
-    ## Replace psu_tags. Example: oct-2024-psu -> jan-2025-psu
-    file_data_str = file_data_str.replace(old_psu_tag, psu_tag)
+    today = datetime.now().strftime(DATE_FORMAT)
 
-    # Write the updated contents back to the file
-    with file_path.open("w") as f:
-        f.write(file_data_str)
+    major_version = version.split(".")[0]
+    minor_version = version.split(".")[1]
+    security_version = version.split(".")[2].split("+")[0]
+    build_version = version.split("+")[1]
 
-    logger.info(
-        f"Updated {file_path} with new full version {version} and psu_tag {psu_tag}"
+    new_file_contents = template.render(
+        major=major_version,
+        minor=minor_version,
+        security=security_version,
+        build=build_version,
+        psu_tag=psu_tag,
+        timestamp=today,
     )
+
+    # Get rid of the build number and replace '.' with '_'
+    formatted_version = version.split("+")[0].replace(".", "_")
+    new_filename = f"jdk_{formatted_version}.json"
+    new_file_path = Path(folder).joinpath(new_filename)
+
+    with open(new_file_path, "w") as new_file:
+        new_file.write(new_file_contents)
+    logger.info(f"Created new JSON file at {new_file_path}")
+
+    return new_file_path
 
 
 def update_shasum(
@@ -209,15 +145,14 @@ def update_shasum(
 
 
 def update_all_shasums(
-    file_path: str,
+    file_path: Path,
 ):
     """
     Update the shasums in the new JSON file.
     Args:
-        file_path (str): The path to the new JSON file.
+        file_path (Path): The path to the new JSON file.
     """
     # Load the contents of the new JSON file into a dictionary
-    file_path = Path(file_path)
     with file_path.open("r") as f:
         file_data = json.load(f)
 
@@ -241,26 +176,19 @@ def main(
     new_version: str,
     psu_tag: str,
 ):
-    new_file_path = create_new_json_file(
-        folder=folder,
-        new_version=new_version,
-    )
-
-    old_version, old_psu_tag = update_new_json_info(
+    new_file_path = create_new_json(
         version=new_version,
-        file_path=new_file_path,
-    )
-
-    update_new_json_full_versions(
-        version=new_version,
-        old_version=old_version,
         psu_tag=psu_tag,
-        old_psu_tag=old_psu_tag,
-        file_path=new_file_path,
+        folder=folder,
     )
 
     update_all_shasums(
         file_path=new_file_path,
+    )
+
+    update_index_file(
+        folder=folder,
+        new_filename=new_file_path.name,
     )
 
     logger.info(
